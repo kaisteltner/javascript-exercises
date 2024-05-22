1102bfdcde0dad7da43be9833dc7a90d402e633c
Kai Steltner <steltner.kai@gmail.com>
2024-05-22 07:29:13 +0200

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayIn, ...elementsToBeRemoved) {
+    let arrayOut = [];
+    outerLoop: for (element of arrayIn) {
+        for (elementToBeRemoved of elementsToBeRemoved) {
+            if (element === elementToBeRemoved) {continue outerLoop};
+        }
+        arrayOut.push(element);
+    }
+    return arrayOut
 };
 
 // Do not edit below this line
