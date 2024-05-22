1149e1f351efcf6008df577b71eac5fc32a916ed
Kai Steltner <steltner.kai@gmail.com>
2024-05-22 08:20:24 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof(start) !== 'number' || typeof(end) !== 'number') {return 'ERROR'};
+    if (start < 0 || end < 0) {return 'ERROR'};
+    // Check if first number is greater than second and switch if necessary
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    // Add numbers
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
