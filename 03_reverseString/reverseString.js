6fd90eee056b6dfd654f9f0786e2d2f483a470c2
Kai Steltner <steltner.kai@gmail.com>
2024-05-22 07:10:22 +0200

    Complete test 1 of exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newChars = [];
+    for (let i = str.length; i >= 0; i--) {
+        newChars.push(str[i]);
+    }
+    return newChars.join('');
 };
 
 // Do not edit below this line
