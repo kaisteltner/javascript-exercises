512268cebd9e57ba564687f990e87c243e358062
Kai Steltner <steltner.kai@gmail.com>
2024-06-07 22:54:53 +0200

    Complete exercise 9

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // Convert n to number and continue only when n >= 0
+    n = Number(n);
+    if (n < 0) return 'OOPS';
+    
+    // Initial sequence
+    let F = [1, 1];
 
+    // Calculate Fibonacci numbers if n > 1
+    for (let i = 2; i < n; i++) {F.push(F[i - 2] + F[i - 1])};
+    return F[n - 1];
 };
 
 // Do not edit below this line
