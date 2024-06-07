c657a4c639baae76611950ac004049989cb33ba7
Kai Steltner <steltner.kai@gmail.com>
2024-06-07 07:51:06 +0200

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, num) => acc + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, num) => acc * num)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  const nums = Array.from(Array(n + 1).keys());
+  return nums.slice(1).reduce((acc, ni) => acc * ni, 1);
 };
 
 // Do not edit below this line
