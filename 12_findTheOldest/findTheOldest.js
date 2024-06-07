97f0735f2620d685988060d4b22023bbd929fa93
Kai Steltner <steltner.kai@gmail.com>
2024-06-07 23:23:32 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const sortedPeople = people
+    .map((person) => {
+      person.age = Object.hasOwn(person, 'yearOfDeath')
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth;
+      return person;
+    })
+    .sort((a, b) => {
+      return b.age - a.age;
+    });
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
