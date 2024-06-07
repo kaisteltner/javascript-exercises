fd70c407b7f7bd88f97b7b5c37a26b60f2233368
Kai Steltner <steltner.kai@gmail.com>
2024-06-07 22:41:12 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const letters = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    for (let i = 0; i <= Math.floor(letters.length / 2); i++) {
+        if (letters[i] !== letters[letters.length - (i + 1)]) {return false} 
+    }
+    return true
 };
 
 // Do not edit below this line
